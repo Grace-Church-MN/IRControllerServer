046de82666aef70f2fc7d12f3681d2d53b85d68a
amclain21 <36746650+amclain21@users.noreply.github.com>
2020-10-04 16:13:39 -0500

    Changed order of code.

--- index.js
+++ index.js
@@ -7,20 +7,18 @@ const io = require('socket.io')(http);
         app.get('/', function(req, res){
                 res.send('<h1>Office Controller Server</h1>');
         });
-        http.listen(3000, function(){
-                console.log('Listening on *:3000');
-        });
-
         io.on('connection', function(socket){
                 console.log('a user connected');
-                //socket.emit('update', settings);
-				socket.on('message', data => {
+				socket.on('message', function(data) {
 					console.log(data);
 				});
 
                 socket.on('disconnect', () => {
                         console.log('user disconnected');
                 });
+        });
+		http.listen(3000, function(){
+                console.log('Listening on *:3000');
         });
 
 })();
