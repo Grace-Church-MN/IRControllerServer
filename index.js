9a194cdf8c5e173c26202c740a06c571ad1d8513
amclain21 <36746650+amclain21@users.noreply.github.com>
2020-10-04 12:35:10 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+(async () => {
+
+        app.get('/', function(req, res){
+                res.send('<h1>Office Controller Server</h1>');
+        });
+        http.listen(3000, function(){
+                console.log('Listening on *:3000');
+        });
+
+        io.on('connection', function(socket){
+                console.log('a user connected');
+                //socket.emit('update', settings);
+
+                socket.on('control led', (data) => {
+                        //settings.led[data.channel] = data.color;
+                        //socket.broadcast.emit('update', settings);
+                        //updateLED();
+                })
+
+                socket.on('disconnect', () => {
+                        console.log('user disconnected');
+                });
+        });
+
+})();
