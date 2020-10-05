84ce493b9be28337a80b4733f23ba278bd394de1
amclain21 <36746650+amclain21@users.noreply.github.com>
2020-10-04 22:06:24 -0500

    added exec to control the different instances of lircd

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const { exec } = require('child_process');
 
 (async () => {
 
-        app.get('/', function(req, res){
-                res.send('<h1>IR Controller Server</h1>');
-        });
-        io.on('connection', function(socket){
-                console.log('a user connected');
-				socket.on('message', function (message) {
-					console.log(message);
-				})
+	app.get('/', function(req, res){
+		res.send('<h1>IR Controller Server</h1>');
+	});
+	io.on('connection', function(socket){
+		console.log('a user connected');
+		socket.on('message', function (message) {
+			if(message.id){
+				console.log(exec('/usr/bin/irsend SEND_ONCE --device=/var/run/lirc/lircd' + message.id + 'Vizio KEY_POWER'));
+			}
+		});
 
-                socket.on('disconnect', () => {
-                        console.log('user disconnected');
-                });
-        });
-		http.listen(3000, function(){
-                console.log('Listening on *:3000');
-        });
+		socket.on('disconnect', () => {
+			console.log('user disconnected');
+		});
+	});
+	http.listen(3000, function(){
+		console.log('Listening on *:3000');
+	});
 
 })();
