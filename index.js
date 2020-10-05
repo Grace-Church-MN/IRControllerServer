976bdcce1c76fe43b3a269c3ead223e9a0d3cceb
amclain21 <36746650+amclain21@users.noreply.github.com>
2020-10-04 22:39:50 -0500

    fixed spelling on message

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { exec } = require('child_process');
 	io.on('connection', function(socket){
 		console.log('a user connected');
 		socket.on('message', function (message) {
-			mesage.forEach((item, i) => {
+			message.forEach((item, i) => {
 				if(item.id){
 					console.log(item.id);
 					if(item.id == "0"){
